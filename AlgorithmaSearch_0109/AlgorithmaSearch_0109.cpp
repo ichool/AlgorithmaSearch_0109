8a1d700921b4f7b8d5f8d1c585b9fa509f3ece04
ichool <faisalaja2653@gmail.com>
2024-04-05 11:00:05 +0700

    membuat operasi if while

--- AlgorithmaSearch_0109/AlgorithmaSearch_0109.cpp
+++ AlgorithmaSearch_0109/AlgorithmaSearch_0109.cpp
@@ -52,7 +52,15 @@ void LinearSearch()
             }
             i++; // step 4
         }
+        if (i == n) // step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons :" << ctr << endl;
 
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+
+    } while ((ch == 'y') || (ch == 'Y'));
+}
 
 int main()
 {
